6372497a95335b0203a99a91d5c6ffd5e324d9f4
Carolina <caroll.zambelli@gmail.com>
2024-04-10 10:18:39 -0300

    setup

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ import awswrangler as wr
 
 
 def handler(event, context):
-    print("aqui")
-    #sql = "SELECT * FROM cadastro"
-    #df = wr.athena.read_sql_query(sql, database='database')
-    #print(df)
+    sql = "SELECT * FROM cadastro"
+    df = wr.athena.read_sql_query(sql, database='database')
+    print(df)
